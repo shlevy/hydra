0b40fc3d287363bfe48cea9bb135a7fa8b9b4905
Shea Levy <shea@shealevy.com>
2013-03-15 13:23:49 -0400

    RESTify the NixChannel base controller.
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- src/lib/Hydra/Base/Controller/NixChannel.pm
+++ src/lib/Hydra/Base/Controller/NixChannel.pm
@@ -33,22 +33,26 @@ sub getChannelData {
 }
 
 
-sub closure : Chained('nix') PathPart {
+sub closure : Chained('nix') PathPart : ActionClass('REST') { }
+
+sub closure_HEAD {
+    my ($self, $c) = @_;
+    $c->response->content_type('application/x-nix-export');
+}
+
+sub closure_GET {
     my ($self, $c) = @_;
     $c->stash->{current_view} = 'NixClosure';
 
     getChannelData($c, 1);
 
     # FIXME: get the closure of the selected path only.
-
-    # !!! quick hack; this is to make HEAD requests return the right
-    # MIME type.  This is set in the view as well, but the view isn't
-    # called for HEAD requests.  There should be a cleaner solution...
-    $c->response->content_type('application/x-nix-export');
 }
 
 
-sub manifest : Chained('nix') PathPart("MANIFEST") Args(0) {
+sub manifest : Chained('nix') PathPart("MANIFEST") Args(0) : ActionClass('REST') { }
+
+sub manifest_GET {
     my ($self, $c) = @_;
     $c->stash->{current_view} = 'NixManifest';
     $c->stash->{narBase} = $c->uri_for($c->controller('Root')->action_for("nar"));
@@ -56,39 +60,58 @@ sub manifest : Chained('nix') PathPart("MANIFEST") Args(0) {
 }
 
 
-sub pkg : Chained('nix') PathPart Args(1) {
+sub pkg : Chained('nix') PathPart Args(1) : ActionClass('REST') {
     my ($self, $c, $pkgName) = @_;
 
     if (!$c->stash->{build}) {
-        $pkgName =~ /-(\d+)\.nixpkg$/ or notFound($c, "Bad package name.");
+        unless ($pkgName =~ /-(\d+)\.nixpkg$/) {
+            $self->status_not_found($c, message => "Bad package name.");
+            return;
+        }
+
         # FIXME: need to handle multiple outputs: channelBuilds is
         # joined with the build outputs, so find() can return multiple
         # results.
-        $c->stash->{build} = $c->stash->{channelBuilds}->find({ id => $1 })
-            || notFound($c, "No such package in this channel.");
+        unless($c->stash->{build} = $c->stash->{channelBuilds}->find({ id => $1 })) {
+            $self->status_not_found($c, message => "No such package in this channel.");
+            return;
+        }
     }
 
     unless (all { isValidPath($_->path) } $c->stash->{build}->buildoutputs->all) {
-        $c->response->status(410); # "Gone"
-        error($c, "Build " . $c->stash->{build}->id . " is no longer available.");
+        $self->status_gone(
+            $c,
+            message => "Build " . $c->stash->{build}->id . " is no longer available."
+        );
     }
+}
+
+sub pkg_HEAD {
+    my ($self, $c) = @_;
+    $c->response->content_type('application/nix-package');
+}
+
+sub pkg_GET {
+    my ($self, $c) = @_;
 
     $c->stash->{manifestUri} = $c->uri_for($self->action_for("manifest"), $c->req->captures);
 
     $c->stash->{current_view} = 'NixPkg';
-
-    $c->response->content_type('application/nix-package');
 }
 
 
-sub nixexprs : Chained('nix') PathPart('nixexprs.tar.bz2') Args(0) {
+sub nixexprs : Chained('nix') PathPart('nixexprs.tar.bz2') Args(0) : ActionClass('REST') { }
+
+sub nixexprs_GET {
     my ($self, $c) = @_;
     $c->stash->{current_view} = 'NixExprs';
     getChannelData($c, 1);
 }
 
 
-sub binary_cache_url : Chained('nix') PathPart('binary-cache-url') Args(0) {
+sub binary_cache_url : Chained('nix') PathPart('binary-cache-url') Args(0) : ActionClass('REST') { }
+
+sub binary_cache_url_GET {
     my ($self, $c) = @_;
     $c->stash->{'plain'} = { data => $c->uri_for('/') };
     $c->response->content_type('text/plain');
@@ -110,14 +133,19 @@ sub sortPkgs {
 }
 
 
-sub channel_contents : Chained('nix') PathPart('') Args(0) {
+sub channel_contents : Chained('nix') PathPart('') Args(0) : ActionClass('REST') { }
+
+sub channel_contents_GET {
     my ($self, $c) = @_;
     # Optimistically assume that none of the packages have been
     # garbage-collected.  That should be true for the "latest"
     # channel.
     getChannelData($c, 0);
     $c->stash->{template} = 'channel-contents.tt';
-    $c->stash->{nixPkgs} = [sortPkgs @{$c->stash->{nixPkgs}}];
+    $self->status_ok (
+        $c,
+        entity => [map { { %$_, build => { $_->{build}->get_columns } }; } (sortPkgs @{$c->stash->{nixPkgs}})]
+    );
 }
 
 
