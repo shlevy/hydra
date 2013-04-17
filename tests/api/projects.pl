c641c206a1c466ef901fc5568dee3cd0f3aee29c
Shea Levy <shea@shealevy.com>
2013-04-17 13:54:10 -0400

    Fix api/projects test
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- tests/api/projects.pl
+++ tests/api/projects.pl
@@ -1,20 +1,23 @@
 use strict;
 use Setup;
+use JSON;
 
 use Test::Simple tests => 4;
 
 createBaseJobset("api-1", "release.nix");
 createBaseJobset("api-2", "default.nix");
 
-my $res = request_json({ uri => "/projects/tests" });
+my $res = request_json({ uri => "/project/tests" });
+
+print $res->as_string();
 
 ok($res->code() == 200, "Can get existing project");
 
 my $data = decode_json($res->content);
 
-ok($data->{project}->{name} == "tests", "The project's name is tests");
+ok($data->{project}->{name} eq "tests", "The project's name is tests");
 
-foreach my $jobset ($data->{jobsets}) {
+foreach my $jobset (@{$data->{jobsets}}) {
     if ($jobset->{name} eq "api-1" or $jobset->{name} eq "api-2") {
         ok(1, "Found jobset maned $jobset->{name}");
     }
